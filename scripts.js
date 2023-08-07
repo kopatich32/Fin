f87cb07039bfe37d44db9d750d7587308eddbeda
kopatich32 <kopatich1994@yandex.ru>
2023-08-08 02:27:00 +0300

    .

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //Anchor animation
 
-let arrow = document.querySelector('a[href="#top"]')
+let arrow = document.querySelector('a[href="#top"]');
     arrow.addEventListener('click', function(event){
     event.preventDefault();
     document.querySelector('#top').scrollIntoView({
@@ -10,9 +10,9 @@ let arrow = document.querySelector('a[href="#top"]')
 })
 
 // Hamburger Menu
-let wrapperBurger = document.querySelector('.menu')
-let hamburgerButton = document.querySelector('.burger')
-let verticalMenu = document.querySelector('header')
+let wrapperBurger = document.querySelector('.menu');
+let hamburgerButton = document.querySelector('.burger');
+let verticalMenu = document.querySelector('header');
 
 
 // document.addEventListener('click', function(event){
@@ -27,26 +27,28 @@ let verticalMenu = document.querySelector('header')
 // })
 
 wrapperBurger.addEventListener('click', function(event){
-    let has = hamburgerButton.classList.toggle('choose')
-    if(has) { verticalMenu.style.left = '0px'
+    let has = hamburgerButton.classList.toggle('choose');
+    if(has) { verticalMenu.style.left = '0px';
     }else{
-        verticalMenu.style.left = ''
+        verticalMenu.style.left = '';
     }
-    event.stopPropagation()
+    event.stopPropagation();
 })
 
 document.onclick = notMenu
 function notMenu(e){
     if(!verticalMenu.contains(e.target)){
-        verticalMenu.style.left = null
-        hamburgerButton.classList.remove('choose')
+        verticalMenu.style.left = null;
+        hamburgerButton.classList.remove('choose');
     }
 }
 
-// Registration form
-let form = document.forms.registration
-let wrapperForm = document.querySelector('.registration_wrapper')
-let accBtn = document.querySelector('.account')
+// Log in form
+let form = document.forms.registration;
+let wrapperForm = document.querySelector('.registration_wrapper');
+let accBtn = document.querySelector('.account');
+let regButton = document.querySelector('button[name="sign_in"]')
+console.log(regButton)
 
 document.addEventListener('click', (e)=>{
     if(accBtn.contains(e.target)){
@@ -56,7 +58,14 @@ document.addEventListener('click', (e)=>{
     }
 })
 
-
+// Registration part
+regButton.addEventListener('click', function (){
+    document.querySelector('form p').innerHTML = 'Регистрация';
+    let elem = document.querySelector('.top')
+    console.log(elem)
+    elem.style.display = null
+    elem.classList.add('display')
+})
 
 
 
