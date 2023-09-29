9be99c5d083e4be9f03e3bd6df17408ac93840e7
kopatich32 <kopatich1994@yandex.ru>
2023-09-29 11:09:58 +0300

    fixed percent field

--- register.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('db.php');
-$login = $_POST['name'];
-$lastName = $_POST['lastname'];
-$password = $_POST['password'];
-$email = $_POST['email'];
-
-$sql = "INSERT INTO `Users` (login, password, email) VALUES ('$login', '$password','$email')";
-$conn -> query($sql);
