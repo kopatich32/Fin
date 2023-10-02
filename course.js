ee3e56fcd9ae102783c4959d27be715236deb1c1
kopatich32 <kopatich1994@yandex.ru>
2023-10-02 22:11:25 +0300

    added watch, edited date area

--- course.js
+++ course.js
@@ -1,3 +1,4 @@
+// course
 fetch('https://www.cbr-xml-daily.ru/latest.js')
     .then(response => response.json())
     .then(data => {
@@ -11,3 +12,9 @@ function forValue(cash){
     sell_doll.innerText =  (+buy_usd.innerText * 1.040603852160333).toFixed(2)
     sell_eur.innerText = (+buy_eur.innerText * 1.040603852160333).toFixed(2)
 }
+// watch
+let time = document.querySelector('.time_now')
+function showTime() {
+    time.innerHTML = new Date().toLocaleTimeString() + ' МСК'
+}
+setInterval(showTime, 1000);
--- dateAndCourse.js
+++ dateAndCourse.js
@@ -1,10 +1,15 @@
 
 let currentValue = document.querySelector('.current_date')
 let date = new Date;
-let allMonth = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+let allMonth = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"]
 function getMonth (month){
     return allMonth[month.getMonth()]
 }
+
+console.log(allMonth[4])
 let thisYear = date.getFullYear();
 let thisDay = date.getDate();
-currentValue.innerHTML ='Сегодня ' +  thisYear + '-' + getMonth(date) + '-' + thisDay
+if(thisDay < 10 ){
+    thisDay = '0'+thisDay
+}
+currentValue.innerHTML =`Сегодня ${thisDay}-е ${getMonth(date)} ${thisYear}`
